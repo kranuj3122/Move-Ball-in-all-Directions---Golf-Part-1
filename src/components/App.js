2802c2282671ab48db5f92f5ac58a16c65193e9a
kranuj3122 <kumaranuj22799@gmail.com>
2021-02-19 02:11:49 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -42,47 +42,33 @@ import "../styles/App.css";
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [posi, setPosi] = useState(0);
-  const [ballPosition, setBallPosi] = useState({ left: 0, top: 0 });
+//   const [ballPosition, setBallPosi] = useState({ left: 0 });
+
   const buttonClickHandler = () => {
     setRenderBall(true);
   };
+
+  const eventHandler = (event) => {
+    switch (event.keyCode) {
+      case 39:
+        setPosi((posi) => posi + 5);
+        break;
+      default:
+        break;
+    }
+  };
+
   useEffect(() => {
-    document.addEventListener("keydown", (event) => {
-      switch (event.keyCode) {
-        case 37:
-          setBallPosi({
-            left: ballPosition.left - 5,
-            top: ballPosition.top,
-          });
-          break;
-        case 38:
-          setBallPosi({
-            left: ballPosition.left,
-            top: ballPosition.top - 5,
-          });
-          break;
-        case 39:
-          setBallPosi({
-            left: ballPosition.left + 5,
-            top: ballPosition.top,
-          });
-          break;
-        case 40:
-          setBallPosi({
-            left: ballPosition.left,
-            top: ballPosition.top + 5,
-          });
-      }
-    });
+    document.addEventListener("keydown", eventHandler);
   }, []);
+
   const renderBallOrButton = () => {
     if (renderBall) {
       return (
         <div
           className="ball"
           style={{
-            left: ballPosition.left + "px",
-            top: ballPosition.top + "px",
+            left: posi + "px",
             position: "absolute",
           }}
         ></div>
